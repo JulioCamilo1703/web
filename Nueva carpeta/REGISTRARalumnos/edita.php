32612ec8a3a5d2294006de33651c1283ad7c3b74
JulioCamilo1703 <148605150+JulioCamilo1703@users.noreply.github.com>
2024-02-02 14:22:02 -0500

    Add files via upload

---
+++ Nueva carpeta/REGISTRARalumnos/edita.php
@@ -0,0 +1,32 @@
+<?php
+require '../../config/database.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['id'], $_POST['nombre'], $_POST['telf'], $_POST['correo'], $_POST['edad'])) {
+
+        $id = $conn->real_escape_string($_POST['id']);
+        $NombreApellido = $conn->real_escape_string($_POST['nombre']);
+        $Telefono = $conn->real_escape_string($_POST['telf']);
+        $Correo = $conn->real_escape_string($_POST['correo']);
+        $Edad = $conn->real_escape_string($_POST['edad']);
+
+        $sql = "UPDATE estudiantes SET nombreApellido=?, telf=?, correo=?, edad=? WHERE id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssi", $NombreApellido, $Telefono, $Correo, $Edad, $id);
+
+        if ($stmt->execute()) {
+            // Operación de actualización exitosa
+            header('Location: ../../instituto/REGISTRARalumnos/estudiantes.php');
+            exit();
+        } else {
+            // Registro de error en producción, imprime en desarrollo
+            error_log("Error al actualizar el registro: " . $stmt->error);
+            echo "Hubo un error al actualizar el registro. Por favor, inténtalo de nuevo más tarde.";
+        }
+
+        $stmt->close();
+    } else {
+        echo "Datos del formulario no presentes";
+    }
+}
+?>
---
+++ Nueva carpeta/registroExitoso.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Exitoso</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/all.min.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row vh-100 justify-content-center align-items-center">
+            <div class="col-md-6 col-sm-10 col-12 bg-white p-4 rounded">
+                <h2 class="text-center mb-4">Registro Exitoso</h2>
+                <div class="text-center">
+                    <a href="/paginaInicioDocente.php" class="btn btn-primary"><i>Siguiente</i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
